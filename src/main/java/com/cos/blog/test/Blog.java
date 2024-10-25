43d677fc52211ba727dc05a45fc7256b09b77578
JongYeal Choi <kcf279@daum.net>
2024-10-26 00:54:16 +0900

    Blog 환경세팅

---
+++ src/main/java/com/cos/blog/test/Blog.java
@@ -0,0 +1,14 @@
+package com.cos.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Blog {
+	@GetMapping("/blog")
+	public String blog() {
+		return "<h1>Hello spring Blog </h1>";
+	}
+
+}
